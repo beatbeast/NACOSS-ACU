032c6b4aeb22635aecf68728551a978567d988e4
Ayomide <bolagbemi@gmail.com>
2020-01-15 10:35:22 +0100

    NACOSS-ACU Website and Voting Platform

--- votesystem/admin/includes/conn.php
+++ votesystem/admin/includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'nacoss');
+	$conn = new mysqli('localhost', 'root', '', 'lss_acu');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
